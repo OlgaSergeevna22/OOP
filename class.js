0dc2ccdf438928f91d334357a735021ac32c41bb
Olga Geres <olgageres13@gmail.com>
2023-03-07 11:32:06 +0300

    change

--- class.js
+++ class.js
@@ -7,53 +7,48 @@
 // // 2) С помощью метода map получить массив, в котором будут содержаться только адреса магазинов. То есть:
 // // ['ул. Петра Мстиславца 11, Минск', 'пр-т Дзержинского, 126, Минск']
 
-function Shop(title, address) {
-    this.title = title;
-    this.address = address;
-}
-
-let green = new Shop('Green', 'ул. Петра Мстиславца 11, Минск');
-console.log(green);
-
-let proStore = new Shop('ProStore', 'пр-т Дзержинского, 126, Минск');
-console.log(proStore);
-
-let shops = [];
-shops.push(green, proStore);
-console.log(shops);
-let arrAddress = shops.map(item => item.address)
-console.log(arrAddress);
-
-
-// // С методами в конструкторе
 
 // function Shop(title, address) {
 //     this.title = title;
 //     this.address = address;
 
-//     this.createGreen = function() {
-//         return shopGreen = {
-//             title: this.title,
-//             address: this.address
-//         }
-
-//     };
-//     this.createProStore = function() {
-//         return this.shopProStore = {
-//             title: this.title,
-//             address: this.address
-//         }
-//     }
 // }
 
 // let green = new Shop('Green', 'ул. Петра Мстиславца 11, Минск');
-// console.log(green.createGreen());
+// console.log(green);
 
 // let proStore = new Shop('ProStore', 'пр-т Дзержинского, 126, Минск');
-// console.log(proStore.createProStore());
+// console.log(proStore);
 
 // let shops = [];
-// shops.push(green.createGreen(), proStore.createProStore());
+// shops.push(green, proStore);
 // console.log(shops);
 // let arrAddress = shops.map(item => item.address)
 // console.log(arrAddress);
+
+
+// // С методами в конструкторе
+
+function Shop(title, address) {
+    this.title = title;
+    this.address = address;
+
+    this.createShop = function() {
+        return {
+            title: this.title,
+            address: this.address
+        }
+    };
+}
+
+let green = new Shop('Green', 'ул. Петра Мстиславца 11, Минск');
+console.log(green.createShop());
+
+let proStore = new Shop('ProStore', 'пр-т Дзержинского, 126, Минск');
+console.log(proStore.createShop());
+
+let shops = [];
+shops.push(green.createShop(), proStore.createShop());
+console.log(shops);
+let arrAddress = shops.map(item => item.address)
+console.log(arrAddress);
